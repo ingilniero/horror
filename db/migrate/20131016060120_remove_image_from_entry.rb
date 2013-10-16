87fa67061a46ec6f26e7858e6d325f732530d3ba
Serge Morales <i.serge23@gmail.com>
2013-10-16 01:25:40 -0500

    Made last migration reversible

--- db/migrate/20131016060120_remove_image_from_entry.rb
+++ db/migrate/20131016060120_remove_image_from_entry.rb
@@ -1,7 +1,7 @@
 class RemoveImageFromEntry < ActiveRecord::Migration
   def change
-    remove_column :entries, :image
-    remove_column :entries, :description
-    add_column    :entries, :content, :string
+    remove_column :entries, :image,       :string
+    remove_column :entries, :description, :string
+    add_column    :entries, :content,     :string
   end
 end
